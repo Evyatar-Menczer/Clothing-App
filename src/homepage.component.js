0ad74b960b8e7b6e595ce5fee486c4729881f749
Evyatar-Menczer <evyatar.menczer@gmail.com>
2022-02-27 23:00:33 +0200

    First commit

---
+++ src/homepage.component.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div className='home-page'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1
+                        className='title'
+                        att1='hello'
+                    >
+                        What The Fuck
+                        Oded ???????
+                    </h1>
+
+                    <span className='subtitle'>
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
